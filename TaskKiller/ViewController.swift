a36b51df50292bd6c11278b474feee45d8afbef2
Oleg Tokmachov <olegtokmachov@mac-mini-oleg.local>
2018-11-07 19:18:48 +0300

    Initial Commit

---
+++ TaskKiller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskKiller
+//
+//  Created by Oleg Tokmachov on 07.11.2018.
+//  Copyright © 2018 Oleg Tokmachov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

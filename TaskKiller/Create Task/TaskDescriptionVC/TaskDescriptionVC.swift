0c879a2bbb442ec19608fe731e6f9ae824cd1487
Oleg Tokmachov <oleg.tokmachov@icloud.com>
2018-11-09 06:31:34 +0300

    added implementation of class TaskDescriptionVC

--- TaskKiller/Create Task/TaskDescriptionVC/TaskDescriptionVC.swift
+++ TaskKiller/Create Task/TaskDescriptionVC/TaskDescriptionVC.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class TaskDescriptionVC: TaskDescriptionReporting {
+class TaskDescriptionVC: UIViewController, TaskDescriptionReporting, UITextFieldDelegate {
+    
+    private let emptyTaskDescription = ""
     
     private var taskDescriptionReceiver: TaskDescriptionReceiving!
+    @IBOutlet weak var taskDescriptionTextField: UITextField! {
+        didSet {
+            taskDescriptionTextField.delegate = self
+            taskDescriptionReceiver.receiveTaskDescription(emptyTaskDescription)
+        }
+    }
     
+    //MARK: TaskDescriptionReporting
     func setTaskDescriptionReceiver(_ receiver: TaskDescriptionReceiving) {
         self.taskDescriptionReceiver = receiver
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        taskDescriptionTextField.resignFirstResponder()
+        return true
+    }
+    @IBAction func didEditedText(_ textField: UITextField) {
+        let taskDescription = textField.text ?? "Error of typing"
+        taskDescriptionReceiver.receiveTaskDescription(taskDescription)
     }
 }

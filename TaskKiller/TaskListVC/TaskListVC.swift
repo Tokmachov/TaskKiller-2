bedc114f40916acc4b315865e8616ca9967c7c55
Oleg Tokmachov <oleg.tokmachov@icloud.com>
2018-11-09 07:13:21 +0300

    added TaskListVC and func createFetchResultsController()

--- TaskKiller/TaskListVC/TaskListVC.swift
+++ TaskKiller/TaskListVC/TaskListVC.swift
@@ -10,12 +10,20 @@ import UIKit
 import CoreData
 
 class TaskListVC: UITableViewController {
-    private var fetchRequestController: NSFetchedResultsController!
-
+    private var fetchRequestController: NSFetchedResultsController<Task>!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchRequestController = createFetchResultsController()
+    }
+    
 }
 extension TaskListVC {
-    private func createFetchResultsController -> NSFetchedResultsController {
+    private func createFetchResultsController() -> NSFetchedResultsController<Task> {
         let fetchResuest: NSFetchRequest<Task> = Task.fetchRequest()
-        //let sortDescriptor = NSSortDescriptor(
+        let sortDescriptor = NSSortDescriptor(key: "dateCreated", ascending: true)
+        fetchResuest.sortDescriptors = [sortDescriptor]
+        let fetchRequestController = NSFetchedResultsController(fetchRequest: fetchResuest, managedObjectContext: PersistanceService.context, sectionNameKeyPath: nil, cacheName: "TaskCache")
+        guard let _ = try? fetchRequestController.performFetch() else { fatalError() }
+        return fetchRequestController
     }
 }

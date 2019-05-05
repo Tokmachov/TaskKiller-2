553d068dd0e6dbfd26b699b6b1a588df85ede65a
Oleg <oleg.tokmachov@icloud.com>
2019-05-05 12:49:53 +0300

    removed comments

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("Hello")
         let notificationCenter = UNUserNotificationCenter.current()
         notificationCenter.requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {(granted, error) in
                 // handle result
